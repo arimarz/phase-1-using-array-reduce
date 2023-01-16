7e3220a707aa252855603ddde1a21e2f83787b14
Ari <aritheriver@gmail.com>
2023-01-16 13:45:55 -0800

    done

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
+const totalBatteries= batteryBatches.reduce(function(count, currentBatch){
+    return count + currentBatch
+})
+//     const summed = batteryBatches.reduce(function(accu, element){
+//         return element + accu}, 0);
+//     if (currentBatch in count) {
+//         count[currentBatch]++;
+//     } else {
+//         count[currentBatch]=1;
+//     }
+//     return count
+// }, {});
+// const summed = batteryBatches.reduce(function(accu, element){
+//     return element + accu}, 0);
+console.log(totalBatteries);
+// console.log(summed);
